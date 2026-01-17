81715bfb3ffc9a201df2bb497ae72959d41f4312
Collin P <collinprusse@mac.com>
2026-01-16 22:42:37 -0700

    fixing dark mode

--- components/ServiceCard/index.js
+++ components/ServiceCard/index.js
@@ -8,10 +8,11 @@ const ServiceCard = ({ name, description }) => {
   useEffect(() => {
     setMounted(true);
   }, []);
+  
   return (
     <div
       className={`w-full p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${
-        mounted && theme === "dark" ? "hover:bg-slate-800" : "hover:bg-slate-50"
+        mounted && theme === "dark" ? "hover:bg-slate-800" : "hover:bg-slate-800"
       } hover:scale-105 link`}
     >
       <h1 className="text-3xl">{name ? name : "Heading"}</h1>
